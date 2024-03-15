4bc4074ebaa1e3c536d3146aa478fc042bf13967
RattleRatchet <Mikael91hoseth@hotmail.com>
2024-03-15 09:31:54 +0100

    Create MQTT_Client.py
    Connect to a MQTT-broker, recieve and publish messages.

---
+++ MQTT_Client.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("test")
+    
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def on_publish(client, userdata, result):
+    print("published")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set(username="Username", password="Password")
+client.on_publish = on_publish
+client.connect("URL/IP", 1883, 60)
+client.subscribe("test")
+client.publish("test", "Ok")
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
